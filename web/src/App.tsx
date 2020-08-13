ea78f3256385a9116e98bb6c77f864dab58b274f
Marcos Leal <marcosleal@gmail.com>
2020-08-13 16:42:01 -0400

    finally adding web - before it was a submodule(?)

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Routes from './routex';
+
+import './assets/styles/global.css';
+
+
+// JSX = Javascript + XML
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
